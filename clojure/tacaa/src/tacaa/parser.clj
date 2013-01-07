6097ac68be9c07054b6e24b78743f00024af9b2c
mageofmath <mageofmath@Tolarian-Academy.(none)>
2013-01-07 10:37:54 -0800

    Added comments throughout the file and a couple TODOs. Tried to add docstrings where I could.

--- clojure/tacaa/src/tacaa/parser.clj
+++ clojure/tacaa/src/tacaa/parser.clj
@@ -1,3 +1,9 @@
+;;Syntax notes:
+;;^Type is a type hint for java interop. It indicated the following identifier should be the indicated type. [^String file] indicates that file should be a string.
+
+;;TODO make 59 not hardcoded everywhere.
+
+
 (ns tacaa.parser
   (:import (java.io InputStream FileInputStream)
            (se.sics.isl.transport Transportable)
@@ -15,22 +21,31 @@
 (def day (atom -1))
 
 (defn mk-game-parser
+  "Input: A string indicating a filename and a reader for that file. If the reader is not provided a default LogReader is created.
+  Output: A Parser object for the given file with the ability to update the messages atom as indicated in the below functions."
   ([^String file] (mk-game-parser file (new LogReader (new FileInputStream file))))
   ([^String file reader]
+      "Proxy is used to make a class with the Parser and reader class types implemented using the following definitions of abstract functions"
      (proxy [Parser] [reader]
        (message [from to content]
+       "Adds to the messages atom the new message including the day the message we sent, the content, the sender, and the recipient.
+       Inputs: String indicating the sender, recipient, and content.
+       Output: The new value of message."
                 (swap! messages conj [from to @day content]))
        (dataUpdated
+       "When called with three arguments updates the messages vector, but not when called with two arguments."
         ([agent type value]
            (when (instance? UserPopulationState value)
              (swap! messages conj [agent type @day value])))
         ([type content] ()))
-       (nextDay [date serverTime] (reset! day date)))))
+       (nextDay 
+       "Sets day to be the indicated first argument. second argument seems ignored" 
+       [date serverTime] (reset! day date)))))
 
 
 (defn mk-queryspace
-  "Returns a query space given a retail catalog"
-  [retail]
+  "Returns a query space given a retail catalog. That is, for each item in the retailer's catelog, gets its manufactuer and component and find all possible queries that could match that item.
+  In this way it returns a collection of queries that could match any item in the retailer's catalog."
   (reduce (fn [coll ^Product val] (let [man (.getManufacturer val)
                                        comp (.getComponent val)
                                        f1cq (new Query nil comp)
@@ -39,6 +54,8 @@
                                    (reduce conj coll [f1cq f1mq f2q]))) #{(new Query)} retail))
 
 (defn remove-nan-bids
+  "Input: The bidbundle currently being generated and the last bidbundle that was generated. As well as a list of possible queries.
+    Output: If any queries, ads, or budgets are invalid, replaces them with the value that was used in the last bundle. Then returns the resulting bidbundle."
   ([^BidBundle bundle ^BidBundle lastbundle queryspace]
      (let [^BidBundle newbundle (reduce (fn [^BidBundle coll ^Query val]
                                           (do
@@ -64,6 +81,8 @@
              (.setCampaignDailySpendLimit newbundle (.getCampaignDailySpendLimit lastbundle))
              (.setCampaignDailySpendLimit newbundle total-budget))
            newbundle)))
+           ;;Overloaded version of the previous function where the defaults are instead 0.0 for all query budgets, ads displayed for each query that 
+           ;;were not previously set are set to being general, and the overall budget if not properly set is set to the maximum possible.
   ([^BidBundle bundle queryspace]
      (let [^BidBundle newbundle (reduce (fn [^BidBundle coll ^Query val]
                                           (do
@@ -100,6 +119,8 @@
         bundle)))
 
 (defn check-bid-bundles
+  "Input: status of a game
+  Output: ensures that there are 59 bidbundles in the bidbundle collection part of the game status."
   [status]
   (let [bid-bundles (status :bid-bundle)]
     (assoc status :bid-bundle
@@ -107,6 +128,7 @@
                      (assoc coll agent
                             (let [bundles (bid-bundles agent)
                                   numbundles (count bundles)]
+                                  ;;What does this do if numbundles > 59 or < 0. TODO
                               (if (== numbundles 59)
                                 bundles
                                 (reduce conj
@@ -118,47 +140,49 @@
 (defn parse-messages
   "Parses all of the messages into a game status"
   ([status messages]
-     (if (seq messages)
+     (if (seq messages) ;;if messages is non-empty.
        (recur
         (let [[from to day content] (first messages)
               from ((status :partic-names) from)
               to ((status :partic-names) to)]
           (cond
-           (instance? BidBundle content) (if (< day 59)
-                                           (if (status :bid-bundle)
+           (instance? BidBundle content) (if (< day 59) ;;the day of the current message
+                                           (if (status :bid-bundle) ;;the bid-bundle exists in the status.
                                              (let [bid-bundle (status :bid-bundle)]
-                                               (if (bid-bundle from)                                          
+                                               (if (bid-bundle from)  ;;from exists in the bid-bundle                           
                                                  (let [bundles (bid-bundle from)
                                                        numbundles (count bundles)
                                                        lastbundle (peek bundles)
-                                                       newbundle (remove-nan-bids content
+                                                       newbundle (remove-nan-bids content ;;newbundle of bids being added.
                                                                                   lastbundle
                                                                                   (status :query-space))]
                                                    (if (< day numbundles) ;too many bundles
                                                      (assoc status :bid-bundle
                                                             (assoc bid-bundle from
-                                                                   (conj (pop bundles) newbundle))) ;remove one
+                                                                   (conj (pop bundles) newbundle))) ;remove one, then add the newbundle. TODO how do you know only 1 is enough to remove?
                                                      (assoc status :bid-bundle
                                                             (assoc bid-bundle from
                                                                    (reduce conj
                                                                            bundles ;replicate bundles as needed
                                                                            (conj (vec (replicate (- day numbundles)
-                                                                                                 lastbundle))
+                                                                                                 lastbundle)) ;extend the last bundle out to the day currently being added.
                                                                                  newbundle))))))
                                                  (let [newbundle (remove-nan-bids content (status :query-space))]
                                                    (assoc status :bid-bundle
                                                           (assoc bid-bundle from
                                                                  (reduce conj []
                                                                          (conj (vec (replicate day
-                                                                                               (mk-empty-bundle)))
+                                                                                               (mk-empty-bundle))) ;;put empty bundles in all the days before this one. Then add the bidbundle.
+                                                                                               ;;TODO remove duplicate code between this and the other if case... I'm sure there is some.
                                                                                newbundle)))))))
-                                             (let [newbundle (remove-nan-bids content (status :query-space))]
+                                             (let [newbundle (remove-nan-bids content (status :query-space))] ;;bid-bundle does not exist in status.
                                                (assoc status :bid-bundle
                                                       {from (reduce conj []
                                                                   (conj (vec (replicate day
-                                                                                        (mk-empty-bundle)))
-                                                                        newbundle))})))
-                                           (let [bid-bundle (status :bid-bundle)
+                                                                                        (mk-empty-bundle))) 
+                                                                        newbundle))}))) ;;adds enough empty bundles to the status to account for previous days up til the newbundle's day.
+                                                                        ;;Then adds the newbundle to the bid-bundle collection stored in the status.
+                                           (let [bid-bundle (status :bid-bundle) ;;day is not < 59.
                                                  bundles (bid-bundle from)
                                                  numbundles (count bundles)
                                                  lastbundle (peek bundles)]
@@ -167,8 +191,8 @@
                                                       (assoc bid-bundle from
                                                              (reduce conj
                                                                      bundles ;replicate bundles as needed
-                                                                     (vec (replicate (- 59 numbundles)
-                                                                                     lastbundle)))))
+                                                                     (vec (replicate (- 59 numbundles) ;;replicate the last bid-bundle out to day 59. and ignore the content.
+                                                                                     lastbundle))))) 
                                                status)))
            (instance? SalesReport content) (if (> day 1)
                                              (if (status :sales-report)
@@ -176,7 +200,7 @@
                                                  (if (sales-report to)
                                                    (assoc status :sales-report
                                                           (assoc sales-report to
-                                                                 (conj (sales-report to) content)))
+                                                                 (conj (sales-report to) content))) ;;adds the content to the sales-report for to. initializes if necessary.
                                                    (assoc status :sales-report
                                                           (assoc sales-report to [content]))))
                                                (assoc status :sales-report {to [content]}))
@@ -187,7 +211,7 @@
                                                  (if (query-report to)
                                                    (assoc status :query-report
                                                           (assoc query-report to
-                                                                 (conj (query-report to) content)))
+                                                                 (conj (query-report to) content))) ;;adds the content to the query report. initializes collections if necessary.
                                                    (assoc status :query-report
                                                           (assoc query-report to [content]))))
                                                (assoc status :query-report {to [content]}))
@@ -198,23 +222,24 @@
                                                 (if (bank-stat to)
                                                   (assoc status :bank-stat
                                                          (assoc bank-stat to
-                                                                (conj (bank-stat to) content)))
+                                                                (conj (bank-stat to) content))) ;;adds the content to the bank status. initializes collections if necessary.
                                                   (assoc status :bank-stat
                                                          (assoc bank-stat to [content]))))
                                               (assoc status :bank-stat {to [content]}))
                                             status)
            (instance? UserPopulationState content) (if (> day 0)
-                                                     (let [retail (seq (status :retail-cat))
+                                                     (let [retail (seq (status :retail-cat)) ;;retail = new seq(status.get(:retail-cat))
                                                            contvec (reduce (fn [coll val]
                                                                              (assoc coll val
                                                                                     (seq (. ^UserPopulationState content (getDistribution ^Product val)))))
-                                                                           {} (seq retail))]
-                                                       (if (status :user-pop)
+                                                                           {} (seq retail))] ;;associates each product in the catalog with its distribution in a map.
+                                                       (if (status :user-pop) ;;if status already contains a UserPopulationState
                                                          (assoc status :user-pop
-                                                                (conj (status :user-pop) contvec))
-                                                         (assoc status :user-pop [contvec])))
-                                                     status)
-           (instance? SlotInfo content) (if (status :slot-info)
+                                                                (conj (status :user-pop) contvec)) ;;add this state to the vector.
+                                                         (assoc status :user-pop [contvec]))) ;;otherwise create the vector.
+                                                     status) ;;return status if the day <= 0.
+            ;;The following 4 only put their Info into the status if the status does not yet contain Info of the appropriate type.
+           (instance? SlotInfo content) (if (status :slot-info) 
                                           status
                                           (assoc status :slot-info content))
            (instance? ReserveInfo content) (if (status :reserve-info)
@@ -223,6 +248,10 @@
            (instance? PublisherInfo content) (if (status :publish-info)
                                                status
                                                (assoc status :publish-info content))
+           (instance? UserClickModel content) (if (status :user-click)
+                                                status
+                                                (assoc status :user-click content))
+            ;;if the RetailCatalog is already in the status, don't change status, otherwise set the retail catalog and associated variable in the status.
            (instance? RetailCatalog content) (if (status :retail-cat)
                                                status
                                                (let [queryspace (mk-queryspace content)]
@@ -233,17 +262,15 @@
                                                                                                              QueryType/FOCUS_LEVEL_ONE)) queryspace))
                                                                 :qsf2 (into #{} (filter (fn [^Query query] (= (.getType query)
                                                                                                              QueryType/FOCUS_LEVEL_TWO)) queryspace))})))
-           (instance? UserClickModel content) (if (status :user-click)
-                                                status
-                                                (assoc status :user-click content))
+           ;;Creates a map of advertiser info if it doesn't exist already, otherwise adds to an existing map. Uses the to field of the message to map to content.
            (instance? AdvertiserInfo content) (if (status :adv-info)
                                                 (assoc status :adv-info
                                                        (assoc (status :adv-info) to
                                                               content))
                                                 (assoc status :adv-info {to content}))
-           :else status))
-        (rest messages))
-       (check-bid-bundles status))))
+           :else status)) ;;doesn't modify status if the content is none of the above classes.
+        (rest messages)) ;;recur on the rest of the messages.
+       (check-bid-bundles status)))) ;;if messages is empty, check-bid-bundles before exiting.
 
 (defn reset-parser
   []
